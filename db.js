4ae340b755d71efd06017b2f3c1d77829a00ea24
seanralph <seanralph007@gmail.com>
2025-07-03 17:03:39 +0100

    Prepare backend for production deployment

--- db.js
+++ db.js
@@ -1,18 +1,29 @@
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// const db = new Pool({
+//     user: process.env.PG_USER,
+//     host: process.env.PG_HOST,
+//     database: process.env.PG_DATABASE,
+//     password: process.env.PG_PASSWORD,
+//     port: process.env.PG_PORT,
+// });
+
+// db.connect();
+
+// module.exports = db;
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const db = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' 
+    ? { rejectUnauthorized: false } 
+    : false
 });
 
-db.connect();
-
 module.exports = db;
-
 
 
 // db.js
