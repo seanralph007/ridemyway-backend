eeac2cc4a2255d4c2e1d49fc59084a68150a7c8a
seanralph <seanralph007@gmail.com>
2025-07-03 17:33:38 +0100

    Fix route mounting

--- server.js
+++ server.js
@@ -45,10 +45,6 @@ cron.schedule('* * * * *', async () => {
   }
 });
 
-app.use((req, res) => {
-  res.status(404).json({ message: "Not Found" });
-});
-
 
 app.use(cookieParser());
 app.use(express.json());
@@ -56,6 +52,10 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/rides', require('./routes/rides'));
 app.use('/api/ride-requests', require('./routes/rideRequests'));
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
 
 const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
