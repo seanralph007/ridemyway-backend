f135ee96fc55c79714c3488a4ecf7aea8f445e64
seanralph <seanralph007@gmail.com>
2025-07-11 17:53:52 +0100

    Logout route update

--- routes/auth.js
+++ routes/auth.js
@@ -133,11 +133,16 @@ router.post('/login', async (req, res) => {
   }
 });
 
-// LOGOUT
+// Logout
 router.post('/logout', (req, res) => {
-  res.clearCookie('token');
+  res.clearCookie('token', {
+    httpOnly: true,
+    secure: true,   
+    sameSite: 'none'
+  });
   res.json({ message: 'Logged out successfully' });
 });
+
 
 // GET CURRENT USER
 router.get('/me', authMiddleware, async (req, res) => {
